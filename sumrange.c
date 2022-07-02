d293fe4d8f214ba2de41f261b02978155b024f66
praniil <pparajuli15@gmail.com>
2022-07-02 22:51:06 +0545

    sum of numbers from 1111 to 4444

---
+++ sumrange.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int number=1111, i, sum=0;
+    for(i=1111; i<=4444; i++){
+        sum= sum + i;
+    }
+    printf("the sum of numbers from 1111 to 4444 is:%d", sum);
+    return 0;
+}
